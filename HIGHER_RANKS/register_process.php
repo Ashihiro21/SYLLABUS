bba1f2808a28d547f6e738080679eff03d5d6f04
Gian Haxian <elmitch0315@gmail.com>
2024-03-26 16:06:05 +0800

    Commit

--- HIGHER_RANKS/register_process.php
+++ HIGHER_RANKS/register_process.php
@@ -2,6 +2,11 @@
 session_start();
 require_once '../Database/connection.php';
 
+// Function to validate email format
+function is_valid_email($email) {
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
 // Fetch data from the registration form
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
@@ -12,9 +17,14 @@ $email = $_POST['email'];
 $password = $_POST['password'];
 $position = $_POST['position'];
 
+// Check if email is valid
+if (!is_valid_email($email)) {
+    echo "Invalid email format!";
+    exit; // Stop execution if email is invalid
+}
+
 // Hash password
 $password_hashed = password_hash($password, PASSWORD_DEFAULT);
-
 
 // Insert data into the database
 $sql = "INSERT INTO users (first_name, last_name, department, courses, phone_number, email, password, position) 
@@ -24,4 +34,5 @@ $stmt->bind_param("ssssssss", $first_name, $last_name, $department, $courses, $p
 $stmt->execute();
 
 echo "Registration successful!";
+
 ?>
