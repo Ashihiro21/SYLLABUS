e72d481f2f80edcd6f680499639ce836ef884d57
louismark45 <118905097+louismark45@users.noreply.github.com>
2024-03-27 17:57:51 +0800

    commit

--- HIGHER_RANKS/login.php
+++ HIGHER_RANKS/login.php
@@ -4,6 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <Style>
+        body {
+            background-image: url('../img/DLSUD-rotonda.jpg');
+            background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+        }
+    </style>
 </head>
 <body>
 
