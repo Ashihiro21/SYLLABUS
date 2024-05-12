044dbf899d06dbd4bbbb88bbf4c25c7b5f4e2058
Gian Haxian <elmitch0315@gmail.com>
2024-05-13 07:55:15 +0800

    Commit

--- HIGHER_RANKS/technique.php
+++ HIGHER_RANKS/technique.php
@@ -26,7 +26,7 @@
 <!-- EDIT POP UP FORM LEARNING MODULE TABLE (Bootstrap MODAL) -->
 <div class="modal fade" id="editmodal_module_learning" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel1"
         aria-hidden="true">
-        <div class="modal-dialog" role="document">
+        <div class="modal-dialog" role="document" >
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel1"> Topic Learning Outcomes  </h5>
