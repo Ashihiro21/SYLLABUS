573daa5da77f4ceb880ae4e5b88f95653bf44dae
Gian Haxian <elmitch0315@gmail.com>
2024-05-29 08:43:41 +0800

    Commit

--- HIGHER_RANKS/trip.php
+++ HIGHER_RANKS/trip.php
@@ -1,74 +1,88 @@
 <?php
 session_start();
+
 // Check if the user is logged in, if not, redirect to the login page
 if (!isset($_SESSION['email'])) {
-    header("Location: index.php");
+    header("Location: login.php");
     exit();
 }
 
 include('../Database/connection.php');
 
-$userEmail = $_SESSION['email']; // Renamed to avoid conflict
+$email = $_SESSION['email'];
 
+// Prepare and execute SQL statement
 $sql = "SELECT 
-            u.`first_name`, 
-            u.`last_name`, 
-            u.`department`, 
-            u.`catid`, 
-            u.`phone_number`, 
-            u.`email`, 
-            u.`password`, 
-            p.`name` AS `position`,
-            c.`id` AS `id`,
-            c.`name` AS `category_name`,
-            c.`initial` AS `category_initial`,
-            c.`dean_name` AS `deans`,
-            c.`dean_position` AS `deans_position`,
-            co.`dean_signature` AS `dean_signatures`,
-            co.`cname`,
-            co.`course_department` AS `course_departments`,
-            co.`initial` AS `course_initial`,
-            co.`department_name` AS `course_dept_name`,
-            co.`department_position` AS `dept_position`,
-            co.`dept_signature` AS `dept_signatures`
+            u.first_name, 
+            u.last_name, 
+            u.department, 
+            u.catid, 
+            u.phone_number, 
+            u.email, 
+            u.password, 
+            p.name AS position,
+            c.id AS category_id,
+            c.name AS category_name,
+            c.initial AS category_initial,
+            c.dean_name AS category_dean,
+            c.dean_position AS category_dean_position,
+            c.logo AS category_logo,
+            co.dean_signature AS deans_category_signature,
+            co.cname,
+            co.course_department AS course_department,
+            co.initial AS course_initial,
+            co.department_name AS dept_head,
+            co.department_position AS dept_head_position,
+            co.dept_signature AS dept_head_signature,
+            co.commitee_signature AS dept_commitee_signature
         FROM 
-            `users` AS u 
+            users AS u 
         LEFT JOIN 
-            `position` AS p ON u.`position` = p.`id`
+            position AS p ON u.position = p.id
         LEFT JOIN 
-            `category` AS c ON u.`department` = c.`id`
+            category AS c ON u.department = c.id
         LEFT JOIN
-            `course` AS co ON u.`catid` = co.`id`
+            course AS co ON u.catid = co.id
         WHERE 
-            u.email = '$userEmail'"; // Changed variable name to avoid conflict
+            u.email = ?";
 
-$result = $conn->query($sql);
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $email);
+$stmt->execute();
+$result = $stmt->get_result();
 
 if ($result->num_rows > 0) {
-    // Output data of each row
-    while ($row = $result->fetch_assoc()) {
-        $first_name = $row['first_name'];
-        $last_name = $row['last_name'];
-        $_SESSION['department'] = $row['department']; 
-        $courses = $row['catid'];
-        $phone_number = $row['phone_number'];
-        $userEmail = $row['email']; // Renamed to avoid conflict
-        $password = $row['password'];
-        $position = $row['position'];
-        $category_name = $row['category_name'];
-        $category_initial = $row['category_initial'];
-        $cname = $row['cname'];
-        $course_initial = $row['course_initial'];
-        $course_departments = $row['course_departments'];
-        $category_dean = $row['deans'];
-        $category_dean_position = $row['deans_position'];
-        $dept_head = $row['course_dept_name'];
-        $dept_head_position = $row['dept_position'];
-        $dept_head_signature = $row['dept_signatures'];
-        $deans_category_signature = $row['dean_signatures'];
-    }
-} 
+    // Fetch data
+    $row = $result->fetch_assoc();
+    $_SESSION['department'] = $row['department']; // Add department to session
+    $_SESSION['catid'] = $row['catid']; // Add department to session
+    $first_name = $row['first_name'];
+    $last_name = $row['last_name'];
+    $department = $row['department'];
+    $phone_number = $row['phone_number'];
+    $email = $row['email'];
+    $password = $row['password'];
+    $position = $row['position'];
+    $category_name = $row['category_name'];
+    $category_initial = $row['category_initial'];
+    $cname = $row['cname'];
+    $course_initial = $row['course_initial'];
+    $course_departments = $row['course_department'];
+    $category_dean = $row['category_dean'];
+    $category_dean_position = $row['category_dean_position'];
+    $dept_head = $row['dept_head'];
+    $dept_head_position = $row['dept_head_position'];
+    $dept_head_signature = $row['dept_head_signature'];
+    $deans_category_signature = $row['deans_category_signature'];
+    $commitee_dept_signature = $row['dept_commitee_signature'];
+    $categories_logo = $row['category_logo'];
+} else {
+    $position = "Position not found";
+}
 
+// Close statement and connection
+$stmt->close();
+$conn->close();
 ?>
 
 <?php
@@ -160,12 +174,11 @@ $analysisArray = [
 // Function to retrieve data from the database and return results
 function fetchModuleData($conn) {
     $department = $_SESSION['department'];
-    $query = "SELECT `id`, `module_no`, `title`, `hours`, `department` FROM `module_learning` WHERE 
-    `department` = $department
+    $catid = $_SESSION['catid'];
+    $query = "SELECT `id`, `module_no`, `title`, `hours`, `department`, `catid` FROM `module_learning` WHERE 
+    `department` = $department and `catid` = $catid
     GROUP BY 
-    module_no, hours, department
-    ORDER BY 
-    id ASC";
+    module_no, hours, department";
     
     // Execute query
     $result = $conn->query($query);
@@ -192,9 +205,6 @@ function fetchModuleData($conn) {
 
                 $results_array[] = $row;
             }
-            // Remove first and last index from the array
-            array_shift($results_array);
-            array_pop($results_array);
         }
     } else {
         // Handle query execution error
@@ -204,12 +214,11 @@ function fetchModuleData($conn) {
     return $results_array;
 }
 
-
-
 // Function to generate and execute SQL queries
 function searchDatabase($conn, $array, $category) {
     $department = $_SESSION['department'];
-    $query = "SELECT * FROM course_leaning WHERE `department` = $department";
+    $catid = $_SESSION['catid'];
+    $query = "SELECT * FROM course_leaning WHERE `department` = $department and `catid` = $catid";
 
     $conditions = array();
     foreach ($array as $item) {
@@ -252,9 +261,7 @@ if ($conn->connect_error) {
 }
 
 // Perform searches and store results
-$hoursResult = fetchModuleData($conn, $knowledgeArray, "hours");
-$module_noResult = fetchModuleData($conn, $knowledgeArray, "module_no");
-$titleResult = fetchModuleData($conn, $knowledgeArray, "title");
+$moduleData = fetchModuleData($conn);
 $knowledgeResults = searchDatabase($conn, $knowledgeArray, "topic_learn_out");
 $compressionResults = searchDatabase($conn, $compressionArray, "topic_learn_out");
 $applyResults = searchDatabase($conn, $applyArray, "topic_learn_out");
@@ -265,6 +272,8 @@ $totalKnowledgeItems = count($knowledgeResults);
 
 // Close connection
 $conn->close();
+
+echo $cname;
 
 // Display results in a table
 echo "<table border='2' style='border-collapse:collapse; text-align:center; padding:5px;'>";
@@ -281,7 +290,7 @@ echo "<th style='width:250px; Padding:5px;'>AN</th>";
 echo "</tr>";
 echo "</tr>";
 
-$maxCount = max(count($hoursResult),count($module_noResult),count($titleResult),count($knowledgeResults), count($compressionResults), count($applyResults), count($analysisResults));
+$maxCount = max(count($moduleData), count($knowledgeResults), count($compressionResults), count($applyResults), count($analysisResults));
 
 $totalHours = 0;
 $totalKnowledge = 0;
@@ -290,7 +299,7 @@ $totalApplication = 0;
 $totalAnalysis = 0;
 
 // Iterate over each array and sum up the counts
-$totalHours = count($hoursResult);
+$totalHours = count($moduleData);
 $totalKnowledge = count($knowledgeResults);
 $totalCompression = count($compressionResults);
 $totalApplication = count($applyResults);
@@ -303,51 +312,48 @@ echo "Total count of all results: $totalCount";
 for ($i = 0; $i < $maxCount; $i++) {
     echo "<tr>";
     echo "<td style='padding: 5px;'>";
-      if (isset(($hoursResult[$i]))) {
-        $totalHours = ($hoursResult[$i]['hours']) * 60;
+    if (isset($moduleData[$i])) {
+        $totalHours = ($moduleData[$i]['hours']) * 60;
         echo $totalHours;
-    }else{
+    } else {
         echo "0";
     }
+    echo "</td>";
     echo "<td style='padding: 5px;'>";
-    if (isset($module_noResult[$i])) {
-        echo $module_noResult[$i]['module_no']."<br>".$titleResult[$i]['title'];
-    }else{
+    if (isset($moduleData[$i])) {
+        echo $moduleData[$i]['module_no']."<br>".$moduleData[$i]['title'];
+    } else {
         echo "0";
     }
     echo "<td style='padding: 5px;'>";
     if (isset($knowledgeResults[$i])) {
         echo $knowledgeResults[$i]['topic_learn_out'];
-    }else{
+    } else {
         echo "0";
     }
     echo "</td>";
     echo "<td style='padding: 5px;'>";
     if (isset($compressionResults[$i])) {
         echo $compressionResults[$i]['topic_learn_out'];
-    }else{
+    } else {
         echo "0";
     }
     echo "</td>";
     echo "<td style='padding: 5px;'>";
     if (isset($applyResults[$i])) {
         echo $applyResults[$i]['topic_learn_out'];
-    }else{
+    } else {
         echo "0";
     }
     echo "</td>";
     echo "<td style='padding: 5px;'>";
     if (isset($analysisResults[$i])) {
         echo $analysisResults[$i]['topic_learn_out'];
-    }else{
+    } else {
         echo "0";
     }
-    
+    echo "</td>";
     echo "<td style='padding:5px;'>Waiting</td>";
-
-  
-   
-    
     echo "</tr>";
 }
 
